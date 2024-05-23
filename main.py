92d09858cf974afad3d46e440d1c2dde7a57b5c0
Nastushka29 <135216978+Nastushka29@users.noreply.github.com>
2024-05-23 22:08:52 +0300

    Пример кода побочной формы

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import tkinter as tk
 
+def show():
+    # Создаем главное окно для побочной формы
+    side_root = tk.Tk()
+    side_root.title("Simple Form with One Button")
+
+    # Создаем кнопку и добавляем ее в окно
+    button = tk.Button(side_root, text="Click Me", command=on_button_click, font=("Arial", 12, "bold"), fg="white", bg="blue")
+    button.pack(pady=20)
+
+    # Запускаем главный цикл обработки событий
+    side_root.mainloop()
+
 def on_button_click():
     print("Button clicked")
-
-# Создаем главное окно
-root = tk.Tk()
-root.title("Simple Form with One Button")
-
-# Создаем кнопку и добавляем ее в окно
-button = tk.Button(root, text="Click Me", command=on_button_click, font=("Arial", 12, "bold"), fg="white", bg="blue")
-button.pack(pady=20)
-
-# Запускаем главный цикл обработки событий
-root.mainloop()
