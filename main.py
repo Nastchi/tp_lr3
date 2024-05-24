0c54be4cfd51c4984906e3dca71f1b5958000024
Nastushka29 <135216978+Nastushka29@users.noreply.github.com>
2024-05-25 00:39:43 +0300

    Добавила Аню

--- main.py
+++ main.py
@@ -12,12 +12,15 @@ def on_button3_click():
     # вызов кода алеси
     print("Button 3 clicked")
 
+def on_button4_click():
+    # вызов кода ани
+    print("Button 4 clicked")
 # Создаем главное окно
 root = tk.Tk()
 root.title("Our Form")
 
 # Задаем размер окна
-root.geometry("400x300")
+root.geometry("400x400")
 
 # Устанавливаем цвет фона окна в фиолетовый
 root.configure(bg='#efdeff')
@@ -36,5 +39,7 @@ button_Rufina.pack(pady=10)
 button_Alesya = tk.Button(root, text="Алеля", command=on_button3_click,font=("Arial", 10, "bold"),fg='#020121', bg='#8cb8f5',width=10,height=2)
 button_Alesya.pack(pady=10)
 
+button_Alesya = tk.Button(root, text="Аня", command=on_button4_click,font=("Arial", 10, "bold"),fg='#352900', bg='#fee27e',width=10,height=2)
+button_Alesya.pack(pady=10)
 # Запускаем главный цикл обработки событий
 root.mainloop()
